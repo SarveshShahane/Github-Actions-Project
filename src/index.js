2a235572bfe5067aac62336ef5d75813e002381f
SarveshShahane <shahanesarvesh45@gmail.com>
2025-08-14 15:33:45 +0530

    Created new test file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-const sum = require('./test/sum');
-
+const sum = require("./test/sum");
+const multiply = require("./test/multiplication");
 const expected = 10;
 const ans = sum(6, 4);
-
+const mul = multiply(6, 4);
 console.log("Expected:", expected);
 console.log("Answer:  ", ans);
+console.log("Multiplication:", mul);
 
 if (ans === expected) {
   console.log("✅ Test passed!");
   process.exit(0); // success
 } else {
   console.error("❌ Test failed!");
+  process.exit(1); // failure
+}
+
+if (mul === 24) {
+  console.log("✅ Multiplication test passed!");
+  process.exit(0); // success
+} else {
+  console.error("❌ Multiplication test failed!");
   process.exit(1); // failure
 }
